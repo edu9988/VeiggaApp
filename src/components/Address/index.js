943c40cfb9f21d1ba66a9685ba86216373f17f25
edu9988 <edu.jerm@gmail.com>
2026-02-12 15:07:45 -0300

    Update packages

--- src/components/Address/index.js
+++ src/components/Address/index.js
@@ -9,7 +9,7 @@ export default (props) => {
     <View style={styles.container}>
       <Text style={styles.text}>{address.address} {address.number}{address.floor && `/${address.floor}`}, {address.locality}</Text>
       <Text style={styles.text}>{address.city}, {address.province} - {address.country}</Text>
-      <Text style={styles.text}>{address.zipcode.slice(0,5)}-{address.zipcode.slice(5)}</Text>
+      {address.zipcode !== null && <Text style={styles.text}>{address.zipcode.slice(0,5)}-{address.zipcode.slice(5)}</Text> || <Text style={styles.text}>CEP não fornecido</Text>}
     </View>
   )
 }
